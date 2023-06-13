53be3bcdcd1545f2cbe329d99084354c0219c90a
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-06-13 12:12:20 +0700

    update stopwatch

--- exercise/src/pages/Stopwatch.jsx
+++ exercise/src/pages/Stopwatch.jsx
@@ -1,16 +1,20 @@
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Grid,
-  GridItem,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Button, Flex, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const Stopwatch = () => {
+  const [timer, setTimer] = useState(0);
+  const [interv, setInterv] = useState();
+
+  const onStart = () => {
+    setInterv(
+      setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000)
+    );
+  };
+
   return (
     <Box
       className="container"
@@ -24,12 +28,23 @@ export const Stopwatch = () => {
     >
       <Stack>
         <Flex justify={"center"}>
-          <Text fontSize="6xl">00:00</Text>
+          <Text fontSize="6xl">
+            {Math.floor(timer / 60 < 10)
+              ? "0" + Math.floor(timer / 60)
+              : Math.floor(timer / 60)}
+            :{timer % 60 < 10 ? "0" + (timer % 60) : timer % 60}
+          </Text>
         </Flex>
         <Flex gap={"10px"} justify={"center"}>
-          <Button colorScheme="orange">Start</Button>
-          <Button colorScheme="red">Stop</Button>
-          <Button colorScheme="yellow">Reset</Button>
+          <Button colorScheme="orange" onClick={() => onStart()}>
+            Start
+          </Button>
+          <Button colorScheme="red" onClick={() => clearInterval(interv)}>
+            Stop
+          </Button>
+          <Button colorScheme="yellow" onClick={() => setTimer(0)}>
+            Reset
+          </Button>
         </Flex>
       </Stack>
     </Box>
